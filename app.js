2da16aa431d7952ced869517e34ddc808e589a44
MaxxeW <15221081420@163.com>
2018-09-17 15:08:51 +0800

    Done

--- app.js
+++ app.js
@@ -2,30 +2,18 @@
 new Vue({
     el: '#vue-app', // the root of this instance will be #vue-app, the DOM element on html
     data: {
-        age: 20,
-        a: 0,
-        b: 0
+        available: false,
+        nearby: false
     },
     methods: {
-        /*
-        addToA: function() {
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function() {
-            console.log('addToB');
-            return this.b + this.age;
-        }
-        */
+
     },
     computed: {
-        addToA: function() {
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function() {
-            console.log('addToB');
-            return this.b + this.age;
+        compClasses: function() {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     } 
 });
