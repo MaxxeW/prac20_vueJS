933c299290d0b2f1609f47a5b2e15a376a72382b
MaxxeW <15221081420@163.com>
2018-09-17 18:08:52 +0800

    Lesson-12 Start

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 new Vue({
     el: '#vue-app', // the root of this instance will be #vue-app, the DOM element on html
     data: {
-        error: false,
-        success: false
+        name: 'Max',
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25},
+            { name: 'Mizumi', age: 35},
+            { name: 'Ken', age: 20},
+        ]
     },
     methods: {
 
