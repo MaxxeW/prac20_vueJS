323ba86cd06674380cd7f742537d4f28b0322801
MaxxeW <15221081420@163.com>
2018-09-18 10:44:51 +0800

    Done

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 new Vue({
     el: '#vue-app', // the root of this instance will be #vue-app, the DOM element on html
     data: {
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            { name: 'Ryu', age: 25},
-            { name: 'Mizumi', age: 35},
-            { name: 'Ken', age: 20},
-        ]
+        health: 100,
+        ended: false
     },
     methods: {
-
+        punch: function() {
+            this.health -= 10;
+            if ( this.health <= 0 ) {
+                this.ended = true;
+            } 
+        },
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
+        }
     },
     computed: {
         
