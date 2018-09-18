43c3722512cb5777897db7af8ea9cc41a2018cf7
MaxxeW <15221081420@163.com>
2018-09-18 10:05:27 +0800

    Done

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 new Vue({
     el: '#vue-app', // the root of this instance will be #vue-app, the DOM element on html
     data: {
-        name: 'Max',
         characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
         ninjas: [
             { name: 'Ryu', age: 25},
